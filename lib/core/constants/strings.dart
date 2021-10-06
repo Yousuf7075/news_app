e438e78fe28ee3b7fed8dec902e8f7a52f2e3f75
YousufQtec <yousuf.qtec2020@gmail.com>
2021-10-06 07:25:23 +0600

    Working on initial setup and packging

---
+++ lib/core/constants/strings.dart
@@ -0,0 +1,8 @@
+class Strings {
+  // App
+  static const appTitle = 'News App';
+
+  // HomeScreen
+  static const homeScreenTitle = 'Home Screen';
+  static const homeScreenCenterText = 'Welcome to News App!';
+}
